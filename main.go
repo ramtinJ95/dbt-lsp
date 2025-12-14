2c95d5c26c358711cd905fada39d8ee59f7aba79
Ramtin Javanmardi <ramme95@gmail.com>
2025-12-14 22:38:52 +0100

    Neovim can now connect and start this lsp

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"bufio"
 	"fmt"
+	"log"
 	"os"
 
 	"github.com/ramtinJ95/dbt-lsp/rpc"
@@ -10,15 +11,27 @@ import (
 
 func main() {
 	fmt.Println("hi")
+	logger := getLogger("/home/ramtinj/personal-workspace/dbt-lsp/log.txt")
+	logger.Printf("hey i started")
 
 	scanner := bufio.NewScanner(os.Stdin)
 	scanner.Split(rpc.Split)
 
 	for scanner.Scan() {
 		msg := scanner.Text()
-		handleMessage(msg)
+		handleMessage(logger, msg)
 	}
 }
 
-func handleMessage(_ any) {
+func handleMessage(logger *log.Logger, msg any) {
+	logger.Println(msg)
+}
+
+func getLogger(filename string) *log.Logger {
+	logfile, err := os.OpenFile(filename, os.O_CREATE|os.O_TRUNC|os.O_WRONLY, 0o666)
+	if err != nil {
+		panic("hey, you didnt give me a good file")
+	}
+
+	return log.New(logfile, "[dbt-lsp]", log.Ldate|log.Ltime|log.Lshortfile)
 }
